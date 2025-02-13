2ac8792fe61724cd44b2e31d04eeda348377fc97
Mubashir-07 <mubashirmhd34@protonmail.com>
2025-02-13 23:58:51 +0530

    added nyt to providers

--- app/Services/NewsProviderFactory.php
+++ app/Services/NewsProviderFactory.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Services\NewsProviders\NewsAPIProvider;
+use App\Services\NewsProviders\NewYorkTimesProvider;
 use App\Services\NewsProviders\TheGuardianProvider;
 
 class NewsProviderFactory
@@ -10,13 +11,14 @@ class NewsProviderFactory
 
     /**
      * @param string $provider
-     * @return NewsAPIProvider|TheGuardianProvider
+     * @return NewsAPIProvider|NewYorkTimesProvider|TheGuardianProvider
      */
     public static function make(string $provider)
     {
         return match ($provider) {
-            'newsapi' => new NewsAPIProvider(),
+            'news_api' => new NewsAPIProvider(),
             'the_guardian' => new TheGuardianProvider(),
+            'nyt' => new NewYorkTimesProvider(),
         };
     }
 }
